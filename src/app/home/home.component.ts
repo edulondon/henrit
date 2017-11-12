358fedf050304ab466a78700b8fa20e4b4b5291c
Kargotech Software <monyejoe@gmail.com>
2017-11-12 05:33:47 +0100

    commit three

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {ActivatedRoute, Router, NavigationEnd} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,37 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
+  private fragment: string;
+  public partners: string;
+  public Speakers: string;
+private scrollExecuted: boolean = false;
+  constructor(private route: ActivatedRoute,  private _router:Router) { }
+    gotoHashtag(fragment: string) {
+    let url = '';
+    let urlWithSegments = this._router.url.split('#');
 
-  constructor() { }
+    if(urlWithSegments.length){
+      url = urlWithSegments[0];
+      //console.log(urlWithSegments[]);
+    }
 
-  ngOnInit() {
-  }
+    window.location.hash = fragment;
+    const element = document.querySelector("#" + fragment);
+    if (element) element.scrollIntoView(element);
+}
+
+
+ ngOnInit() {
+  this._router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = this._router.parseUrl(this._router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(element); }
+        }
+      }
+    });
+}
 
 }
+
